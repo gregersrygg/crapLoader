da872f7f94ba1dbabc2bc8a7e07336c077e61e28
Gregers Gram Rygg <gregersrygg@gmail.com>
2010-11-01 21:48:44 +0100

    merge

--- src/crapLoader.js
+++ src/crapLoader.js
@@ -1,152 +1,182 @@
 
 var crapLoader = (function() {
-    var queue = []
+    var initialized = false
+        ,queue = []
         ,inputBuffer = []
         ,writeBuffer = {}
         ,chunkBuffer
-        ,loading = false
+        ,loading = 0
         ,elementCache = {}
         ,splitScriptsRegex = /(<script[^>]+src=['"]?[^'"\s]+[^>]*>\s*<\/script>)/
         ,externalScriptSrcRegex = /<script[^>]+src=['"]?([^'"\s]+)[^>]*>\s*<\/script>/
+        ,globalOptions = {
+            loadSequentially: false,
+            printTree: false
+        }
         ,defaultOptions = {
             async: false,
             charset: "utf-8",
             success: undefined
-        },p = {
+        },priv,publ;
+     
+
+    priv = {
+        checkWriteBuffer: function(obj) {
+            var buffer = writeBuffer[obj.domId];
+
+            if(buffer && buffer.length) {
+                priv.writeHtml( buffer.shift(), obj );
+
+            } else if(queue.length) {
+                console.log("Loading script from queue!");
+                priv.loadScript( queue.shift() );
+
+            }
+        },
+
+        extend: function(t, s) {
+            if(!s) return t;
+            for(var k in s) {
+                t[k] = s[k];
+            }
+            return t;
+        },
+
+        flush: function(obj) {
+            var domId = obj.domId
+               ,outputFromScript
+               ,htmlPartArray;
+
+            outputFromScript = inputBuffer.join("");
+            inputBuffer = [];
             
-            captureDocWrite: function() {
-                
-                document.write = p.writeReplacement;
-                
-            },
+            htmlPartArray = priv.separateScriptsFromHtml( outputFromScript );
             
-            checkWriteBuffer: function(domId) {
-                var buffer = writeBuffer[domId];
-                if(buffer && buffer.length) {
-                    p.writeHtml(buffer.shift(), domId);
-                }
-            },
-            
-            extend: function(t, s) {
-                if(!s) return t;
-                for(var k in s) {
-                    t[k] = s[k];
-                }
-                return t;
-            },
-            
-            flush: function(domId) {
-                //console.log("INPUTBUFFER: " + inputBuffer)
-                var outputFromScript = p.separateScriptsFromHtml( inputBuffer.join("") );
-                inputBuffer = [];
-                
-                if(!writeBuffer[domId]) {
-                    writeBuffer[domId] = outputFromScript;
-                } else {
-                    writeBuffer[domId].unshift.apply(writeBuffer, outputFromScript);
-                }
-                p.checkWriteBuffer(domId);
-            },
-            
-            getElById: function(domId) {
-                return elementCache[domId] || (elementCache[domId] = document.getElementById(domId));
-            },
-            
-            injectToDoc: function(chunk) {
-                
-                if(chunk.s) {
-                    
-                } else {
-                    
-                }
-                
-            },
-            
-            loadScript: function(src, domId, options) {
-                var defaultOptsCopy = p.extend({}, defaultOptions);
-                options = p.extend(defaultOptsCopy, options);
-                options.depth = 0;
-                options.src = src;
-                options.domId = domId;
-                
-                // async loading code from jQuery
-                var head = document.getElementsByTagName("head")[0] || document.documentElement
-                   ,script = document.createElement("script");
-                script.type = "text/javascript";
-                script.charset = options.charset;
-                
-                var done = false;
-                // Attach handlers for all browsers
-				script.onload = script.onreadystatechange = function() {
-					if ( !done && (!this.readyState ||
-							this.readyState === "loaded" || this.readyState === "complete") ) {
-						done = true;
-						script.onload = script.onreadystatechange = null;
-                        if ( head && script.parentNode ) {
-                    		head.removeChild( script );
-            			}
-            			
-            			p.flush(domId);
-            			if(options.success && typeof options.success == "function") {
-            			    options.success.call(script);
-            			}
-            		}
-				};
-				
-                script.src = src;
-				// Use insertBefore instead of appendChild  to circumvent an IE6 bug.
-                // This arises when a base node is used (#2709 and #4378).
-                head.insertBefore( script, head.firstChild );
-                //console.log(head.innerHTML)
-            },
-            
-            onLoadHandlerProxy: function(domId) {
-                
-            },
-            
-            orgWrite: document.write,
-            
-            
-            separateScriptsFromHtml: function(htmlStr) {
-                
-                var chunks = []
-                    ,splitHtml = htmlStr.split(splitScriptsRegex)
-                    ,i = 0
-                    ,l = splitHtml.length
-                    ,scriptMatch
-                    ,tmp;
-                return splitHtml;    
-                /*
-                while(i < l) {
-                    tmp = splitHtml[i++];
-                    if(!tmp) continue;
-                    scriptMatch = tmp.match(/<script[^>]+src=['"]?([^'"\s]+)[^>]*>/);
-                    chunks.push( (scriptMatch && scriptMatch.length==2 ? {s:scriptMatch[1]} : {h:tmp} ) );
-                }
-                
-                return chunks;*/
+            if(!writeBuffer[domId]) {
+                writeBuffer[domId] = htmlPartArray;
+            } else {
+                writeBuffer[domId].unshift.apply(writeBuffer, htmlPartArray);
+            }
+            priv.checkWriteBuffer(obj);
+        },
         
-            },
+        getElById: function(domId) {
+            return elementCache[domId] || (elementCache[domId] = document.getElementById(domId));
+        },
+
+        loadScript: function(obj) {
+            loading++;
+            // async loading code from jQuery
+            var head = document.getElementsByTagName("head")[0] || document.documentElement
+               ,script = document.createElement("script");
+            script.type = "text/javascript";
+            script.charset = obj.charset;
             
-            writeHtml: function(html, domId) {
-                var scriptMatch = html.match(externalScriptSrcRegex);
-                if(scriptMatch && scriptMatch.length == 2) {
-                    var scriptSrc = scriptMatch[1];
-                    p.loadScript(scriptSrc, domId);
-                } else {
-                    var container = p.getElById(domId);
-                    if(!container) throw new Error("crapLoader: Unable to inject html. Element with id '" + domId + "' does not exist");
-                    container.innerHTML += html;
-                    p.checkWriteBuffer(domId);
-                }
-            },
-            
-            writeReplacement: function(str) {
-                //console.log("document.write: " + str);
-                inputBuffer.push(str);
-            
+            if(globalOptions.printTree) {
+                priv.printScriptSrc(obj);
             }
             
-        };
-    return p;
+            var done = false;
+            // Attach handlers for all browsers
+            script.onload = script.onreadystatechange = function() {
+                loading--;
+                script.loaded = true;
+                if ( !done && (!this.readyState ||
+                        this.readyState === "loaded" || this.readyState === "complete") ) {
+                    done = true;
+                    script.onload = script.onreadystatechange = null;
+                    if ( head && script.parentNode ) {
+                        head.removeChild( script );
+                    }
+                    
+                    priv.flush(obj);
+                    if(obj.success && typeof obj.success == "function") {
+                        obj.success.call(script);
+                    }
+                }
+            };
+            
+            script.loaded = false;
+            script.src = obj.src;
+            obj.depth++;
+            
+            // Use insertBefore instead of appendChild  to circumvent an IE6 bug.
+            // This arises when a base node is used (#2709 and #4378).
+            head.insertBefore( script, head.firstChild );
+            setTimeout(function() {
+                if(!script.loaded) console.error("SCRIPT NOT LOADED", script.src);
+            }, 3000);
+        },
+        
+        printScriptSrc: function(obj) {
+            var logoutput = "", i=obj.depth;
+            while(i-- > 1) {
+                logoutput += " ";
+            }
+            if(i-- > 0) {
+                logoutput += "+"
+            }
+            logoutput += "--" + obj.src;
+            console.log(logoutput);
+        },
+        
+        separateScriptsFromHtml: function(htmlStr) {
+            var chunks = []
+                ,splitHtml = htmlStr.split(splitScriptsRegex)
+                ,i = 0
+                ,l = splitHtml.length
+                ,scriptMatch
+                ,tmp;
+            return splitHtml;    
+        },
+
+        writeHtml: function(html, obj) {
+            var scriptMatch = html.match(externalScriptSrcRegex);
+            if(scriptMatch && scriptMatch.length == 2) {
+                var scriptSrc = scriptMatch[1];
+                obj.src = scriptSrc;
+                priv.loadScript(obj);
+            } else {
+                var container = priv.getElById(obj.domId);
+                if(!container) throw new Error("crapLoader: Unable to inject html. Element with id '" + obj.domId + "' does not exist");
+                container.innerHTML += html;
+                priv.checkWriteBuffer(obj);
+            }
+        },
+        
+        writeReplacement: function(str) {
+            //console.log("document.write: " + str);
+            inputBuffer.push(str);
+        
+        }
+
+    };
+    
+    publ = {
+        hijack: function(options) {
+            if(initialized) return;
+            initialized = true;
+            priv.extend(globalOptions, options);
+            
+            document.write = priv.writeReplacement; 
+        },
+         
+        loadScript: function(src, domId, options) {
+            var defaultOptsCopy = priv.extend({}, defaultOptions);
+            var obj = priv.extend(defaultOptsCopy, options);
+            obj.src = src;
+            obj.domId = domId;
+            obj.depth = 0;
+            
+            if(loading && globalOptions.loadSequentially) {
+                queue.push(obj);
+            } else {
+                priv.loadScript(obj);
+            }
+        },
+        
+        orgWrite: document.write,
+     };
+
+    return publ;
 })();
